9d72d886aedde9229985c6b2de6157e8d50e19e8
YashSuthar983 <yashsuthar983@gmail.com>
2025-03-26 23:33:53 +0530

    Fix the Ghost Draw where a curve is drawn which was previously created and added feature where by clicking edge user directed near it.

--- __init__.py
+++ __init__.py
@@ -78,7 +78,13 @@ from venturial.lib.global_properties import VNT_global_properties_collection, VN
 
 from venturial.models.edges_panel_operators import *
 
+
+
+
+
 classes = (
+    VNT_OT_select_edge,
+    VNT_ecustom_edge_obj,
     VNT_user_preferences_collection,
     VNT_OT_save_preferences,
     VNT_OT_reset_preferences,
@@ -363,8 +369,10 @@ def register():
     bpy.types.Scene.fmcustom = CollectionProperty(type=VNT_global_properties_collection) # for face merging
     bpy.types.Scene.fmcustom_index = IntProperty()
 
+    bpy.types.Scene.ecustom_edge_obj = CollectionProperty(type=VNT_ecustom_edge_obj)
     bpy.types.Scene.ecustom = CollectionProperty(type=VNT_global_properties_collection_edge_verts) # for edges
-    bpy.types.Scene.ecustom_index = IntProperty()
+    bpy.types.Scene.ecustom_index = IntProperty(update=update_selected_edge)
+    bpy.types.Scene.last_ecustom_index = IntProperty(default=-1)
 
     bpy.types.Scene.vert_index = IntProperty(name="Vertex Index", default=0)
 
